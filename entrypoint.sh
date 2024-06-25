8563347d157756a5864b99a4af33b93418367dea
Andrew Wang <andy_wang@harvard.edu>
2024-06-25 14:24:02 -0400

    initial ver

--- entrypoint.sh
+++ entrypoint.sh
@@ -1,15 +1,24 @@
-#!/bin/bash
+#!/usr/local/bin/bash -x
 
-LATEST_TAG=$(git describe --tags --abbrev=0)
-MESSAGE=$(git log --format=%B -n 1)
+# LATEST_TAG=$(git describe --tags --abbrev=0)
+# MESSAGE=$(git log --format=%B -n 1)
+
+echo $@
+LATEST_TAG=$1
+MESSAGE=$2
+if [[ "$LATEST_TAG" =~ ^v(.*) ]]; then 
+    NUM=${BASH_REMATCH[1]}; 
+else 
+    NUM=$LATEST_TAG
+fi
 if [[ "$MESSAGE" =~ '[major]' ]]; then
-    NEW_TAG=$(echo $LATEST_TAG | awk -F. -v OFS=. '{$1 += 1 ; $2 = 0; $3 = 0; print}')
+    NEW_TAG=$(echo $NUM | awk -F. -v OFS=. '{$1 += 1 ; $2 = 0; $3 = 0; print}')
 elif [[ "$MESSAGE" =~ '[minor]' ]]; then
-    NEW_TAG=$(echo $LATEST_TAG | awk -F. -v OFS=. '{$2 += 1 ; $3 = 0; print}')
+    NEW_TAG=$(echo $NUM | awk -F. -v OFS=. '{$2 += 1 ; $3 = 0; print}')
 else
-    NEW_TAG=$(echo $LATEST_TAG | awk -F. -v OFS=. '{$3 += 1 ; print}')
+    NEW_TAG=$(echo $NUM | awk -F. -v OFS=. '{$3 += 1 ; print}')
 fi
 
-echo "New tag: $NEW_TAG"
-git tag $NEW_TAG
-git push --tags
+echo "$NEW_TAG"
+# git tag $NEW_TAG
+# git push --tags
